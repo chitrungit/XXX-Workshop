f4495e048d56a51c2a02a759e1ac65733c691c01
chitrungit <XXX@MacMini.local>
2014-08-01 10:30:01 +0700

    init workshop

---
+++ XXX-Workshop/XXX-Workshop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XXX-Workshop
+//
+//  Created by XXX on 8/1/14.
+//  Copyright (c) 2014 XXX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
