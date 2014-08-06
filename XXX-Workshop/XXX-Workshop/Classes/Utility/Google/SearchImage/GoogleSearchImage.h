7661684267336da71604c0d034fd8215348e26a0
chitrungit <XXX@MacMini.local>
2014-08-06 10:21:35 +0700

    add google search image

--- XXX-Workshop/XXX-Workshop/Classes/Utility/Google/SearchImage/GoogleSearchImage.h
+++ XXX-Workshop/XXX-Workshop/Classes/Utility/Google/SearchImage/GoogleSearchImage.h
@@ -8,16 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
+@class GoogleSearchImage;
+
+typedef void(^GoogleSearchImageCompletionBlock)(GoogleSearchImage* search, NSError* error);
+
 @interface GoogleSearchImage : NSObject
+{
+    GoogleSearchImageCompletionBlock _completionBlock;
+}
 
+-(GoogleSearchImage*) initWithKeyword:(NSString*) keyword completionBlock:(GoogleSearchImageCompletionBlock) completionBlock;
 
+-(void) start;
+-(void) cancel;
+
+@property (nonatomic, strong, readonly) NSString *keyword;
+@property (nonatomic, strong) NSMutableArray *result;
+@property (nonatomic, strong, readonly) NSMutableData *data;
 
 @end
 
 @interface GoogleImageObject : NSObject
 
++(GoogleImageObject*) makeWithDictionary:(NSDictionary*) dict;
+
 @property (nonatomic, strong) NSString *thumbnailURL;
 @property (nonatomic, assign) CGSize thumbnailSize;
-//@property (nonatomic, strong) NSString *imageURL:
+@property (nonatomic, strong) NSString *imageURL;
+@property (nonatomic, assign) CGSize imageSize;
 
 @end
--- XXX-Workshop/XXX-Workshop/Classes/Utility/Google/SearchImage/GoogleSearchImage.m
+++ XXX-Workshop/XXX-Workshop/Classes/Utility/Google/SearchImage/GoogleSearchImage.m
@@ -8,6 +8,103 @@
 
 #import "GoogleSearchImage.h"
 
+@interface GoogleSearchImage()<NSURLConnectionDataDelegate>
+{
+    NSURLConnection *_conn;
+}
+
+@end
+
 @implementation GoogleSearchImage
+
+-(GoogleSearchImage *)initWithKeyword:(NSString *)keyword completionBlock:(GoogleSearchImageCompletionBlock)completionBlock
+{
+    self=[super init];
+    
+    _keyword=keyword;
+    _completionBlock=[completionBlock copy];
+    
+    NSString *text=[keyword stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSURL *url=[NSURL URLWithString:[NSString stringWithFormat:@"https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%@&rsz=8&as_filetype=jpg",text]];
+    
+    NSURLRequest *request=[NSURLRequest requestWithURL:url];
+    _conn=[NSURLConnection connectionWithRequest:request delegate:self];
+    
+    return self;
+}
+
+-(void)start
+{
+    [_conn start];
+}
+
+-(void)cancel
+{
+    [_conn cancel];
+}
+
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    if(!_data)
+        _data=[NSMutableData dataWithData:data];
+    else
+        [_data appendData:data];
+}
+
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSError *error=nil;
+    NSDictionary *json=[NSJSONSerialization JSONObjectWithData:_data options:NSJSONReadingAllowFragments|NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:&error];
+    NSArray *images=json[@"responseData"][@"results"];
+    
+    _result=[NSMutableArray array];
+    for(NSDictionary *dict in images)
+    {
+        [_result addObject:[GoogleImageObject makeWithDictionary:dict]];
+    }
+    
+    [self finish:nil];
+}
+
+-(void) finish:(NSError*) error
+{
+    if(_completionBlock)
+        _completionBlock(self,error);
+    
+    _completionBlock=nil;
+    
+    _conn=nil;
+}
+
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    [self finish:error];
+}
+
+-(void)dealloc
+{
+    _completionBlock=nil;
+    
+    if(_conn)
+    {
+        [_conn cancel];
+        _conn=nil;
+    }
+}
+
+@end
+
+@implementation GoogleImageObject
+
++(GoogleImageObject *)makeWithDictionary:(NSDictionary *)dict
+{
+    GoogleImageObject *obj=[GoogleImageObject new];
+    obj.thumbnailURL=dict[@"tbUrl"];
+    obj.thumbnailSize=CGSizeMake([dict[@"tbWidth"] floatValue], [dict[@"tbHeight"] floatValue]);
+    obj.imageURL=dict[@"Url"];
+    obj.imageSize=CGSizeMake([dict[@"Width"] floatValue], [dict[@"Height"] floatValue]);
+    
+    return obj;
+}
 
 @end
