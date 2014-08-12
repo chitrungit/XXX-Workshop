ab491b15462ddbd4b3de7a49566206fb867e32bb
chitrungit <XXX@MacMini.local>
2014-08-12 14:39:19 +0700

    add DynamicHeightTableCell

---
+++ XXX-Workshop/XXX-Workshop/Classes/DynamicHeightTableCell/DynamicHeightTableCell.h
@@ -0,0 +1,13 @@
+//
+//  DynamicHeightTableCell.h
+//  XXX-Workshop
+//
+//  Created by XXX on 8/12/14.
+//  Copyright (c) 2014 XXX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DynamicHeightTableCell : UITableViewCell
+
+@end
---
+++ XXX-Workshop/XXX-Workshop/Classes/DynamicHeightTableCell/DynamicHeightTableCell.m
@@ -0,0 +1,13 @@
+//
+//  DynamicHeightTableCell.m
+//  XXX-Workshop
+//
+//  Created by XXX on 8/12/14.
+//  Copyright (c) 2014 XXX. All rights reserved.
+//
+
+#import "DynamicHeightTableCell.h"
+
+@implementation DynamicHeightTableCell
+
+@end
